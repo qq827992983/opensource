6807f351ed97c9d7bf76cd035e948fbba0513b1b
qq827992983 <qq827992983@126.com>
2014-08-06 16:48:46 +0800

    ownhome:修改sql脚本

--- java/ownHome/src/sql.sql
+++ java/ownHome/src/sql.sql
@@ -1,21 +1,14 @@
-ownhome sql
-log:
-create table log(id int primary key, msg varchar(256) );
+create database if not exists ownhome;
 
-users:
-create table users(userId int primary key auto_increment,userName varchar(100),password varchar(100) ,realName varchar(100),sex varchar(10));
+create table if not exists log(id int primary key auto_increment, msg varchar(256));
 
-article:
-create table article(articleId int primary key,title varchar(100),int type, content varchar(4096), writer varchar(100) writeDate DateTime);
+create table if not exists users(userId int primary key auto_increment,userName varchar(100),password varchar(100) ,realName varchar(100),sex varchar(10));
 
-type:
-create table type(typeId int primary key, type varchar(100));
+create table if not exists type(typeId int primary key auto_increment, type varchar(100));
+
+create table if not exists article(articleId int primary key,title varchar(100), content varchar(4096), writer varchar(100), writeDate DateTime, typeId int, foreign key(typeId) references type(typeId));
+
+create table if not exists book(int id primary key auto_increment,
 
 
-create table emb_t_dictBusType
-(
-   emb_c_busTypeID      int not null auto_increment,
-   emb_c_busTypeEnName  varchar(255) not null,
-   emb_c_busTypeZhName  varchar(255) not null,
-   primary key(emb_c_busTypeID)  
-)engine=INNODB  default charset=gbk;
+name,sex,id,phone,address,mobilePhone,company,comPhone,comAddress,relation,userId(foreign key)
